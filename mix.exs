916ded57a4d4a209f0bfe14f682e3cf96954d50a
Tony Han <h.bing612@gmail.com>
2015-03-06 00:44:29 +0800

    change dep version of plug

--- mix.exs
+++ mix.exs
@@ -18,11 +18,11 @@ defmodule OAuth2.Mixfile do
 
   defp deps do
     [
-      {:hackney, "~> 0.14.1"},
-      {:httpoison, "~> 0.5.0"},
-      {:poison, "~> 1.2"},
+      {:hackney, "~> 1.0.6"},
+      {:httpoison, "~> 0.6.0"},
+      {:poison, "~> 1.3.1"},
       {:cowboy, "~> 1.0", only: :test},
-      {:plug, "~> 0.9.0"},
+      {:plug, "~> 0.11.0"},
     ]
   end
 
